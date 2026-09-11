50c2e13591099f84cbcaa83e5452a1217d7dabd8
ynyejin <yyj030625@gmail.com>
2026-09-11 12:29:28 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/lingcast/server/LingcastServerApplication.java
@@ -0,0 +1,13 @@
+package com.lingcast.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingcastServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LingcastServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lingcast/server/LingcastServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lingcast.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LingcastServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
